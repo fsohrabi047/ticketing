5d80a4253424234a50c99fc64a7e6078147ac105
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-24 18:26:10 +0330

    added signin feature

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,8 +3,8 @@ import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import {validateRequest} from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -19,13 +19,8 @@ router.post(
           .isLength({ min: 4, max: 20 })
           .withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-      }
-
       const { email, password } = req.body;
 
       const existingUser = await User.findOne({ email });
@@ -38,7 +33,6 @@ router.post(
       await user.save();
 
       // Generate JWT
-
       const userJwt = jwt.sign(
           {
             id: user.id,
