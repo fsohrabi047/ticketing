35ba178ad813a36117b16dbb88bd898e82ea1c2d
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-24 17:53:15 +0330

    improved request validation exception

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,13 @@
 import { ValidationError } from 'express-validator';
 import { CustomError } from './custom-error';
+import {FieldValidationError} from "express-validator/src/base";
+import * as path from "path";
 
+type ValidationErr = Partial<ValidationError> & { path?: string, value?: string }
 export class RequestValidationError extends CustomError {
   statusCode = 400;
 
-  constructor(public errors: ValidationError[]) {
+  constructor(public errors: ValidationErr[]) {
     super('Invalid request parameters');
 
     // Only because we are extending a built in class
@@ -12,10 +15,8 @@ export class RequestValidationError extends CustomError {
   }
 
   serializeErrors() {
-    return this.errors.map(err => {
-      // return { message: err.msg, field: err.param };
-
-      return { message: err.msg, field: 'Fix this error' };
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.path, value: err.value };
     });
   }
 }
