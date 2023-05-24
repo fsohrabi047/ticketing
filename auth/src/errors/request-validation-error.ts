fe1a99a9d1657490f9b5bb4e8990c0922146e4bd
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-24 16:51:28 +0330

    added signup feature

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,20 +1,21 @@
-import { ValidationError } from "express-validator";
-import { CustomError } from "./custom-error";
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
 export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super("Invalid request parameters");
+    super('Invalid request parameters');
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 
   serializeErrors() {
-    return this.errors.map((err) => {
+    return this.errors.map(err => {
       // return { message: err.msg, field: err.param };
-      return { message: err.msg, field: 'ffffffffffffffffffffffffffffff' };
+
+      return { message: err.msg, field: 'Fix this error' };
     });
   }
 }
