4ed87e5f7474266e7a9aa61b3dbe3df7cf3f8aba
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-27 22:12:27 +0330

    install common module and fixed issues

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,8 +3,7 @@ import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
-import {validateRequest} from "../middlewares/validate-request";
+import { BadRequestError, validateRequest } from '@far_ticketing/common';
 
 const router = express.Router();
 
