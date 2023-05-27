412fe16a37b03fd67226555bb9d46ac6dffacc0e
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-27 21:57:16 +0330

    added errors and middlewares to common module

--- common/src/middlewares/require-auth.ts
+++ common/src/middlewares/require-auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import {NotAuthorizedError} from "../errors/not-authorized";
+import {NotAuthorizedError} from "../errors/not-authorized-err";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
     if (!req.currentUser) {
